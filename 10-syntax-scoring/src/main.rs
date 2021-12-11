91e86678efc827e1fb5b8a089e16967013b82a72
Mike Reinhard <mike@mikereinhard.com>
2021-12-10 23:45:21 -0800

    syntax scoring part 2

--- 10-syntax-scoring/src/main.rs
+++ 10-syntax-scoring/src/main.rs
@@ -16,25 +16,45 @@ fn main() {
         .map(|s| s.unwrap())
         .collect();
 
-    let mut score = 0;
+    let mut corrupted_score = 0;
+    let mut autocomplete_scores = vec![];
     for row in rows {
         let mut syntax = vec![];
+        let mut is_corrupted = false;
         for c in row.chars() {
             match c {
                 '(' | '[' | '{' | '<' => syntax.push(c),
                 ')' | ']' | '}' | '>' => {
                     let last = syntax.pop().unwrap();
                     if last != get_bracket_pair(c) {
-                        score += get_corrupt_score(c);
+                        corrupted_score += get_corrupt_score(c);
+                        is_corrupted = true;
                         break;
                     }
                 },
                 _ => ()
             }
         }
+        
+        // println!("{} {:?}", is_corrupted, syntax);
+        if !is_corrupted {
+            let mut autocomplete_score = 0;
+            while !syntax.is_empty() {
+                let next = syntax.pop().unwrap();
+                autocomplete_score = get_new_autocomplete_score(
+                    next, autocomplete_score
+                );
+            }
+            autocomplete_scores.push(autocomplete_score);
+        }
     }
 
-    println!("Corrupted score is {}", score);
+    autocomplete_scores.sort();
+    let middle = (autocomplete_scores.len() + 1) / 2 - 1;
+    // println!("{:?} {}", autocomplete_scores, middle);
+
+    println!("Corrupted score is {}",  corrupted_score);
+    println!("Middle autocomplete score is {}",  autocomplete_scores[middle]);
 }
 
 fn get_bracket_pair(c: char) -> char {
@@ -43,6 +63,10 @@ fn get_bracket_pair(c: char) -> char {
         ']' => '[',
         '}' => '{',
         '>' => '<',
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
         _ => c
     }
 }
@@ -53,6 +77,17 @@ fn get_corrupt_score(c: char) -> u32 {
         ']' => 57,
         '}' => 1197,
         '>' => 25137,
+        _ => 0
+    }
+}
+
+fn get_new_autocomplete_score(c: char, score: u64) -> u64 {
+    let new_score = score * 5;
+    new_score + match get_bracket_pair(c) {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
         _ => 0
     }
 }
